af68de3cd94011c432a0b425a2f7cd9e1401e898
nubowski <nubowski@nnteam.ru>
2023-05-11 00:26:57 +0300

    feat: add new simple UI output for metal

--- js/game.js
+++ js/game.js
@@ -14,7 +14,6 @@ let producers = [metalDrill];
 // Game Loop
 setInterval(update, 1000);
 
-
 // Update function
 function update() {
     // Each produce on every prod
@@ -25,8 +24,31 @@ function update() {
         }
     }
 
+    // Update the UI
+    updateUI();
+
     // Some debug
     console.log(`Metal: ${metal.quantity}`);
+}
+
+function updateUI() {
+    // get containers
+    let resourceContainer = document.getElementById('resources-container');
+    let productionContainer = document.getElementById('production-container');
+
+    // clear containers
+    resourceContainer.innerHTML = '';
+    productionContainer.innerHTML = '';
+
+    // add current state of resources to container
+    resourceContainer.innerHTML += `<p>${metal.name}: ${metal.quantity.toFixed(2)}</p>`;
+
+    // add current state of producers to container
+    for (let producer of producers) {
+        productionContainer.innerHTML += `<p>${producer.name} (Level ${producer.level}): ` +
+        `Produces ${producer.productionRate.toFixed(2)} per second. ` +
+        `Cost for next upgrade: ${producer.baseCost * Math.pow(1.07, producer.level)}</p>`;
+    }
 }
 
 
