f725f9460d9baefd9bf9d7621328deda23c10e0d
nubowski <nubowski@nnteam.ru>
2023-05-12 05:31:39 +0300

    feat: add constant DEFAULT_CONSTRUCTION_TIME (ms just yet)

--- js/classes/constants.js
+++ js/classes/constants.js
@@ -3,3 +3,4 @@ export const MAX_LOG_ENTRIES = 100;
 export const MAX_BUILDING_SPACE = 20;
 export const DEFAULT_BUILDING_LEVEL = 1;
 export const DEFAULT_QUEUE_SIZE = 3;
+export const DEFAULT_CONSTRUCTION_TIME = 5000; // 1000ms == 1s
